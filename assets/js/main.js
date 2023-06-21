af298357572616f008c73cca583bc98a4a49bffe
Gadivalr <levi.sojos7@gmail.com>
2023-06-20 21:59:02 -0400

    only dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,23 @@
 
 (() => {
-  // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
 
-  const toggleTheme = (state) => {
-    if (state === "dark") {
-      localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
-    } else if (state === "light") {
-      localStorage.setItem("theme", "dark");
-      body.setAttribute("data-theme", "dark");
-    } else {
-      initTheme(state);
-    }
-
-    // Actualizar Disqus después de cambiar el tema
+  const toggleTheme = () => {
+    localStorage.setItem("theme", "dark");
+    body.setAttribute("data-theme", "dark");
     
+    // Actualizar Disqus después de cambiar el tema
   };
 
-  lamp.addEventListener("click", () =>
-    toggleTheme(localStorage.getItem("theme"))
+  toggleTheme(); // Establecer el tema oscuro de forma predeterminada al cargar la página
+
+  lamp.addEventListener("click", () => toggleTheme( 
+    window.location.href = "/" // Actualizar la página después de cambiar el tema
+  )
+      
   );
+  
 
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
